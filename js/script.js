0d2c77ba236ec73ece9a01ee189ccd19c79b1ac7
Guilherme Rodrigues dos Anjos <guilherme.r.dos.anjos@outlook.com.br>
2026-09-13 22:40:28 -0300

    Realizando a limpeza dos dados em JS

--- js/script.js
+++ js/script.js
@@ -11,34 +11,26 @@ const infraestrutura = {
 /* Servidores */
 
 const elementoServidores = document.getElementById("quantidade-servidores");
-console.log(elementoServidores);
-
 elementoServidores.textContent = infraestrutura.servidores;
 
 /* Switches */
-const quantidadeSwitches = 8;
-const elementoSwitches = document.getElementById("quantidade-switches");
-console.log(elementoSwitches);
 
-elementoSwitches.textContent = quantidadeSwitches;
+const elementoSwitches = document.getElementById("quantidade-switches");
+elementoSwitches.textContent = infraestrutura.switches;
 
 /* Ativos */
-const quantidadeAtivos = 50;
-const elementoAtivos = document.getElementById("quantidade-ativos");
-console.log(elementoAtivos);
 
-elementoAtivos.textContent = quantidadeAtivos;
+const elementoAtivos = document.getElementById("quantidade-ativos");
+elementoAtivos.textContent = infraestrutura.ativos;
 
 /* Redes */
-const quantidadeRedes = 4;
+
 const elementoRedes = document.getElementById("quantidade-redes");
-console.log(elementoRedes);
-elementoRedes.textContent = quantidadeRedes;
+elementoRedes.textContent = infraestrutura.redes;
 
 /* Incidentes */
-const quantidadeIncidentes = 8;
+
 const elementoIncidentes = document.getElementById("quantidade-incidentes");
-console.log(elementoIncidentes);
-elementoIncidentes.textContent = quantidadeIncidentes;
+elementoIncidentes.textContent = infraestrutura.incidentes;
 
 console.log(infraestrutura.servidores);
