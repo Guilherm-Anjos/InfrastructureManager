807719d9549ce083905076776d9454bfc7ff6c1e
Guilherme Rodrigues dos Anjos <guilherme.r.dos.anjos@outlook.com.br>
2026-09-13 21:30:17 -0300

    Trabalhando com const

--- js/script.js
+++ js/script.js
@@ -2,3 +2,8 @@ console.log("Script carregado com sucesso!");
 
 const quantidadeServidores = 12;
 console.log(quantidadeServidores);
+
+const elementoServidores = document.getElementById("quantidade-servidores");
+console.log(elementoServidores);
+
+elementoServidores.textContent = quantidadeServidores;
