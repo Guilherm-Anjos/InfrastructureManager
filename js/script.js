4cf6b7e7668048546e56efe92c265261ca5f1019
Guilherme Rodrigues dos Anjos <guilherme.r.dos.anjos@outlook.com.br>
2026-09-13 22:35:15 -0300

    Ajustando as constantes que carregam os valores nos cards.

--- js/script.js
+++ js/script.js
@@ -1,16 +1,44 @@
 console.log("Script carregado com sucesso!");
 
-const quantidadeServidores = 12;
-console.log(quantidadeServidores);
+const infraestrutura = {
+    servidores: 12,
+    switches: 8,
+    ativos: 50,
+    redes: 4,
+    incidentes: 8
+};
+
+/* Servidores */
 
 const elementoServidores = document.getElementById("quantidade-servidores");
 console.log(elementoServidores);
 
-elementoServidores.textContent = quantidadeServidores;
+elementoServidores.textContent = infraestrutura.servidores;
 
+/* Switches */
 const quantidadeSwitches = 8;
 const elementoSwitches = document.getElementById("quantidade-switches");
 console.log(elementoSwitches);
 
 elementoSwitches.textContent = quantidadeSwitches;
 
+/* Ativos */
+const quantidadeAtivos = 50;
+const elementoAtivos = document.getElementById("quantidade-ativos");
+console.log(elementoAtivos);
+
+elementoAtivos.textContent = quantidadeAtivos;
+
+/* Redes */
+const quantidadeRedes = 4;
+const elementoRedes = document.getElementById("quantidade-redes");
+console.log(elementoRedes);
+elementoRedes.textContent = quantidadeRedes;
+
+/* Incidentes */
+const quantidadeIncidentes = 8;
+const elementoIncidentes = document.getElementById("quantidade-incidentes");
+console.log(elementoIncidentes);
+elementoIncidentes.textContent = quantidadeIncidentes;
+
+console.log(infraestrutura.servidores);
