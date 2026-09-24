8b471fbbddd600a6903a82f856fac24a1aaa1e96
Guilherme Rodrigues dos Anjos <guilherme.r.dos.anjos@outlook.com.br>
2026-09-24 12:16:19 -0300

    Iniciando o banco de dados no C#

--- backend/InfrastructureManager.Api/obj/Debug/net10.0/InfrastructureManager.Api.AssemblyInfo.cs
+++ backend/InfrastructureManager.Api/obj/Debug/net10.0/InfrastructureManager.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InfrastructureManager.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ec79a718fb66a849a6ffca667c2c40a19778313")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28a22db71bede926e7c870561b7ee156f98cdaf9")]
 [assembly: System.Reflection.AssemblyProductAttribute("InfrastructureManager.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InfrastructureManager.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
